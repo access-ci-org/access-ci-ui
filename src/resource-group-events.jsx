a41234c53c7de077a77a9afdbf35726815d2ec44
Matt Yoder <mjyoder@psc.edu>
2024-10-28 09:57:16 -0400

    perf(resource-group-events): use events API version 1.1

--- src/resource-group-events.jsx
+++ src/resource-group-events.jsx
@@ -9,18 +9,11 @@ export default function ResourceGroupEvents({ baseUri, infoGroupId }) {
     `${baseUri}/api/resource-groups/${infoGroupId}/announcements.json`,
     null
   );
-  // FIXME: Once events can be looked up by info_groupid, the request to get
-  // affinity group data can be removed.
-  const affinityGroupData = useJSON(
-    `https://support.access-ci.org/api/1.0/affinity_groups/${infoGroupId}`,
+  const eventData = useJSON(
+    `https://support.access-ci.org/api/1.1/events/ag/${infoGroupId}`,
     null,
     { corsProxy: true }
   );
-  const eventUri =
-    affinityGroupData && !affinityGroupData.error && affinityGroupData.length
-      ? `https://support.access-ci.org/api/1.0/events/ag/${affinityGroupData[0].nid}`
-      : null;
-  const eventData = useJSON(eventUri, null, { corsProxy: true });
   const filteredEvents =
     eventData && !eventData.error
       ? eventData.filter((event) => new Date(event.date__start) >= new Date())
