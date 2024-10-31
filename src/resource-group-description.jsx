4bac6f07ccd45526162273f8345481116223c3be
Matt Yoder <mjyoder@psc.edu>
2024-10-31 14:16:57 -0400

    feat(resource-group-description): use CIDeR resource group data

--- src/resource-group-description.jsx
+++ src/resource-group-description.jsx
@@ -1,48 +1,37 @@
-import { htmlToJsx, useJSON } from "./utils";
+import { useResourceGroup } from "./utils";
 
 import Icon from "./icon";
 
 export default function ResourceGroupDescription({ baseUri, infoGroupId }) {
-  const data = useJSON(
-    `https://support.access-ci.org/api/1.0/affinity_groups/${infoGroupId}`,
-    { corsProxy: true }
-  );
-  if (!data || data.error || !data.length) return;
-  const name = data[0].title;
-  const imageUri = `https://support.access-ci.org/${data[0].field_image}`;
-  const description = htmlToJsx(data[0].description);
-  // TODO: Add resource provider and user guide data.
-  const { resourceProvider, userGuideUri } = data;
+  const resourceGroup = useResourceGroup(infoGroupId);
+  if (!resourceGroup) return;
+  const { name, description, imageUri, tags } = resourceGroup;
   return (
     <div class="resource-group-description">
       {imageUri ? (
         <img class="resource-group-feature-image" src={imageUri} />
       ) : null}
-      <h1>{name}</h1>
-      {resourceProvider ? (
-        <p>
-          <a
-            href={`/resource-providers/${resourceProvider.resourceProviderId}`}
-          >
-            {resourceProvider.iconUri ? (
-              <Icon
-                alt={resourceProvider.name}
-                src={resourceProvider.iconUri}
-              />
-            ) : null}
-            {resourceProvider.name}
-          </a>
-        </p>
-      ) : null}
+      <h1>{resourceGroup.name}</h1>
+
+      {tags
+        .filter((tag) => tag.tagCategory.name == "Resource Provider")
+        .map((tag) => (
+          <p>
+            <a href="#">
+              {tag.iconUri ? <Icon alt={tag.name} src={tag.iconUri} /> : null}
+              {tag.name}
+            </a>
+          </p>
+        ))}
       {description ? <p>{description}</p> : null}
       <a href="https://allocations.access-ci.org" class="btn secondary lg">
         <Icon name="check2-circle" /> Get Started with {name}
       </a>
-      {userGuideUri ? (
+      {/* {userGuideUri ? (
         <a href={userGuideUri} class="btn lg">
           <Icon name="book" /> User Guide
         </a>
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
