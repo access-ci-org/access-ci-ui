2e5ddcbf9a7c81eb7e9be57f32d853d2b7b88687
Matt Yoder <mjyoder@psc.edu>
2024-12-03 13:19:13 -0500

    fix(section-navigation): fix navigation in shadow DOM

--- src/section-navigation.jsx
+++ src/section-navigation.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 import Icon from "./icon";
 
 export default function SectionNavigation({ prompt = "Jump To:" }) {
   const [sections, setSections] = useState([]);
+  const container = useRef(null);
+
   const updateSections = () => {
     const newSections = [];
-    for (let heading of document.querySelectorAll("[data-section-title]"))
+    for (let heading of container.current.querySelectorAll(
+      "[data-section-title]"
+    ))
       newSections.push({
         icon: heading.dataset.sectionIcon,
         id: heading.id,
@@ -15,27 +19,45 @@ export default function SectionNavigation({ prompt = "Jump To:" }) {
 
     setSections(newSections);
   };
+
+  const scrollToSection = (sectionId) => {
+    const section = container.current.querySelector(`#${sectionId}`);
+    if (section) section.scrollIntoView();
+  };
+
   useEffect(() => {
+    if (!container.current) return;
     updateSections();
     document.addEventListener("accessci-update-sections", updateSections);
-  }, []);
-  return sections.length >= 2 ? (
-    <nav class="section-navigation">
-      {prompt ? <h2>{prompt}</h2> : null}
-      <ul>
-        {sections.map(({ icon, id, title }) => (
-          <li>
-            <a href={`#${id}`}>
-              {icon ? (
-                <>
-                  <Icon name={icon} />{" "}
-                </>
-              ) : null}
-              {title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  ) : null;
+  }, [container.current]);
+
+  return (
+    <div ref={(el) => (container.current = el?.parentElement)}>
+      {sections.length >= 2 ? (
+        <nav class="section-navigation">
+          {prompt ? <h2>{prompt}</h2> : null}
+          <ul>
+            {sections.map(({ icon, id, title }) => (
+              <li>
+                <a
+                  href={`#${id}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(id);
+                  }}
+                >
+                  {icon ? (
+                    <>
+                      <Icon name={icon} />{" "}
+                    </>
+                  ) : null}
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      ) : null}
+    </div>
+  );
 }
