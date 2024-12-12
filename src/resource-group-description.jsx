5d072a6455ad34d82213645d42f990dd56848c7f
Matt Yoder <mjyoder@psc.edu>
2024-12-12 16:39:39 -0500

    feat(resource-group-description): show user guide button if available

--- src/resource-group-description.jsx
+++ src/resource-group-description.jsx
@@ -1,12 +1,29 @@
-import { useResourceGroup } from "./utils";
+import { useJSON, useResourceGroup } from "./utils";
 
 import Icon from "./icon";
 
 export default function ResourceGroupDescription({ infoGroupId }) {
   const resourceGroup = useResourceGroup(infoGroupId);
-  console.log(resourceGroup);
+  const firstResource = useJSON(
+    resourceGroup && resourceGroup.infoResourceIds.length
+      ? `https://operations-api.access-ci.org/wh2/cider/v1/info_resourceid/${resourceGroup.infoResourceIds[0]}/?format=json`
+      : null
+  );
+
   if (!resourceGroup) return;
-  const { name, description, imageUri, organizations, tags } = resourceGroup;
+  const { accessAllocated, name, description, imageUri, organizations } =
+    resourceGroup;
+
+  const getStartedUri = accessAllocated
+    ? "https://allocations.access-ci.org/get-your-first-project"
+    : null;
+
+  const userGuideUri =
+    firstResource && firstResource.results
+      ? (firstResource.results.compute || firstResource.results.storage || {})
+          .user_guide_url
+      : null;
+
   return (
     <div class="resource-group-description">
       {imageUri ? (
@@ -28,14 +45,21 @@ export default function ResourceGroupDescription({ infoGroupId }) {
         </p>
       ))}
       {description ? <p>{description}</p> : null}
-      <a href="https://allocations.access-ci.org" class="btn secondary lg">
-        <Icon name="check2-circle" /> Get Started with {name}
-      </a>
-      {/* {userGuideUri ? (
-        <a href={userGuideUri} class="btn lg">
-          <Icon name="book" /> User Guide
-        </a>
-      ) : null} */}
+
+      <div style={{ marginTop: "1rem" }}>
+        {getStartedUri ? (
+          <a href={getStartedUri} class="btn secondary lg">
+            <Icon name="check2-circle" /> Get Started with {name}
+          </a>
+        ) : null}
+        {userGuideUri ? (
+          <a href={userGuideUri} class="btn lg">
+            <Icon name="book" /> User Guide
+          </a>
+        ) : null}
+      </div>
+
+      {/*  */}
     </div>
   );
 }
