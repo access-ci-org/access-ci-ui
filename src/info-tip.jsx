53d31d7ba9c02d5ef7cb15a30a99ac585eef7cef
Matt Yoder <mjyoder@psc.edu>
2025-07-11 08:49:31 -0400

    refactor(tooltip): extract tooltip functionality from InfoTip

--- src/info-tip.jsx
+++ src/info-tip.jsx
@@ -1,26 +1,16 @@
-import { useLayoutEffect, useRef } from "preact/hooks";
-import tippy from "tippy.js";
-
 import Icon from "./icon";
+import Tooltip from "./tooltip";
 
 export default function InfoTip({
   color = "var(--contrast-6)",
   icon = "info-circle-fill",
   tooltip,
 }) {
-  const trigger = useRef(null);
-  useLayoutEffect(() => {
-    if (trigger.current)
-      tippy(trigger.current, {
-        content: tooltip,
-        theme: "access",
-        appendTo: trigger.current.parentElement,
-      });
-  }, [trigger.current]);
-
   return (
-    <button class="info-tip" style={{ color }} ref={trigger}>
-      <Icon name={icon} />
-    </button>
+    <Tooltip tooltip={tooltip}>
+      <button class="info-tip" style={{ color }} tabIndex="-1">
+        <Icon name={icon} />
+      </button>
+    </Tooltip>
   );
 }
---
+++ src/tooltip.jsx
@@ -0,0 +1,29 @@
+import { useLayoutEffect, useRef } from "preact/hooks";
+import tippy from "tippy.js";
+
+export default function Tooltip({ children, tooltip }) {
+  const trigger = useRef(null);
+
+  useLayoutEffect(() => {
+    if (!trigger.current) {
+      return;
+    }
+
+    const instance = tippy(trigger.current, {
+      content: tooltip,
+      theme: "access",
+      appendTo: trigger.current.parentElement,
+      // trigger: "mouseenter focus",
+    });
+
+    return () => {
+      instance.destroy();
+    };
+  }, [tooltip]);
+
+  return (
+    <span ref={trigger} tabIndex="0">
+      {children}
+    </span>
+  );
+}
